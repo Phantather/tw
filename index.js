e74bd8feb0de73f33d8f25a5ca175de73881ba89
Phantather <akdil@MacBook-Pro-Akdil.local>
2023-03-03 23:44:50 +0600

    init

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4000
+
+app.set('view engine', 'ejs')
+
+app.use(express.static(__dirname + '/src'));
+
+
+
+// Маршруты
+app.get('/', (req, res) => {
+    res.render('pages/home/index'); // Переход на главную страницу
+});
+//
+app.get('/archive', (req, res) => {
+    res.render('pages/archive/index'); // Переход на страницу "О нас"
+});
+
+app.get('/events', (req, res) => {
+    res.render('pages/events/index'); // Переход на страницу "Контакты"
+});
+
+app.get('/founders', (req, res) => {
+    res.render('pages/founders/index'); // Переход на страницу "Контакты"
+});
+app.get('/history', (req, res) => {
+    res.render('pages/history/index'); // Переход на страницу "Контакты"
+});
+app.get('/mission', (req, res) => {
+    res.render('pages/mission/index'); // Переход на страницу "Контакты"
+});
+//
+// // Запуск сервера
+app.listen(PORT, () => {
+    console.log('Сервер запущен на порту 3000');
+})
---
+++ src/js/main.js
@@ -0,0 +1,74 @@
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('.header');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 0) { // здесь можно изменить расстояние, на котором будет меняться цвет
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+
+
+// Burger
+const burger = document.querySelector('.header__burger')
+const headerList = document.querySelector('.header__list')
+
+burger.addEventListener('click', () => {
+    console.log(
+        'Click'
+    );
+    burger.classList.toggle('open')
+    headerList.classList.toggle('show')
+})
+
+
+//swiper
+
+
+var swiper = new Swiper(".mySwiper",{
+    slidesPerView:4,
+    spaceBetween: 30,
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    // onmousewheel:true,
+    // keyboard:true,
+    autoplay: {
+        delay: 2000
+    },
+    loop:true,
+    breakpoints:{
+        1200:{
+            slidesPerView: 4,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+        992:{
+            slidesPerView:3,
+        },
+        886:{
+            slidesPerView:2,
+        },
+        768:{
+            slidesPerView:2,
+        },
+        676:{
+            slidesPerView:1,
+        },
+        577:{
+            slidesPerView:1,
+        },
+        475:{
+            slidesPerView:1,
+        },
+        425:{
+            slidesPerView:1,
+        },
+        320:{
+            slidesPerView:1,
+        }
+    }
+});
