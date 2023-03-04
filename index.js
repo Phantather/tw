cbc076871d1ff92490159c860fa92eea35772a67
Phantather <akdil@MacBook-Pro-Akdil.local>
2023-03-05 00:28:33 +0600

    add page contact

--- index.js
+++ index.js
@@ -11,33 +11,37 @@ app.use(express.static(__dirname + '/src'));
 
 // Маршруты
 app.get('/', (req, res) => {
-    res.render('pages/home/index'); // Переход на главную страницу
+    res.render('pages/home/index.ejs'); // Переход на главную страницу
 });
 //
 app.get('/archive', (req, res) => {
-    res.render('pages/archive/index'); // Переход на страницу "О нас"
+    res.render('pages/archive/index.ejs'); // Переход на страницу "О нас"
 });
 
 app.get('/events', (req, res) => {
-    res.render('pages/events/index'); // Переход на страницу "Контакты"
+    res.render('pages/events/index.ejs'); // Переход на страницу "Контакты"
 });
 
 app.get('/founders', (req, res) => {
-    res.render('pages/founders/index'); // Переход на страницу "Контакты"
+    res.render('pages/founders/index.ejs'); // Переход на страницу "Контакты"
 });
 app.get('/history', (req, res) => {
-    res.render('pages/history/index'); // Переход на страницу "Контакты"
+    res.render('pages/history/index.ejs'); // Переход на страницу "Контакты"
 });
 app.get('/mission', (req, res) => {
-    res.render('pages/mission/index'); // Переход на страницу "Контакты"
+    res.render('pages/mission/index.ejs'); // Переход на страницу "Контакты"
 });
 
 app.get('/news', (req, res) => {
-    res.render('pages/news/index'); // Переход на страницу "Контакты"
+    res.render('pages/news/index.ejs'); // Переход на страницу "Контакты"
 });
 
 app.get('/gallery', (req, res) => {
-    res.render('pages/gallery/index'); // Переход на страницу "Контакты"
+    res.render('pages/gallery/index.ejs'); // Переход на страницу "Контакты"
+});
+
+app.get('/contact', (req, res) => {
+    res.render('pages/contact/index.ejs'); // Переход на страницу "Контакты"
 });
 //
 // // Запуск сервера
