acbcef0d29a4e4e1570cbf589f373621c84f76ef
bek77g <d.zheenkul@gmail.com>
2023-04-01 19:30:30 +0600

    add: locales save switcher val

--- src/js/global.js
+++ src/js/global.js
@@ -1,6 +1,19 @@
+//Language switcher start
 let languageSelect = document.querySelector('#language-select');
+let languageOption = document.querySelector('#language-select-option');
+
+document.addEventListener('DOMContentLoaded', function () {
+  if (localStorage['lang']) {
+    // if lang is set
+    languageSelect.value = localStorage['lang']; // set the value
+  }
+  languageSelect.onchange = function () {
+    localStorage['lang'] = this.value; // change localStorage on change
+  };
+});
 
 languageSelect.addEventListener('change', ({ target: { value } }) => {
   document.cookie = `lang=${value}`;
   window.location.reload();
 });
+//Language switcher end
