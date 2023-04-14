6405bdbf6b08492f78126743784d0c3943a3e3fa
Phantather <akdil.hstech@gmail.com>
2023-04-14 17:14:19 +0600

    add changes

--- routes/archive.js
+++ routes/archive.js
@@ -1,10 +1,31 @@
 const {Router} = require('express')
 const request = require("request");
+const API = require("../configs/axios");
+const DOMAIN_PATH = require("../configs/variables");
 const router = Router()
 
 
-router.get('/archive', (req, res) => {
-    res.render('pages/archive/index.ejs'); // Переход на страницу "О нас"
+router.get('/archive', async (req, res) => {
+    let response = null;
+    let lng = req.getLocale();
+    let dateQuery = req.query.date;
+    let query = !!dateQuery ? `&filters[Date]=${dateQuery}` : '';
+    try {
+        await API.get(`/events?populate=deep&locale=${lng}${qury}`).then(
+            ({ data }) => (response = data)
+        );
+    } catch (err) {
+        console.log(err);
+    }
+
+    console.log(!!dateQuery, dateQuery, query);
+
+    res.render('pages/archive/index.ejs', {
+        data: {
+            response,
+            domain: DOMAIN_PATH.slice(0, DOMAIN_PATH.length),
+        },
+    }); // Переход на страницу "Контакты"
 });
 
 module.exports = router
