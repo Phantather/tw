273eb3ea703a59dfc6a15857db4a313d28879d5a
Phantather <akdil.hstech@gmail.com>
2023-04-23 12:19:40 +0600

    add: mission page

--- routes/colobarators.js
+++ routes/colobarators.js
@@ -1,10 +1,23 @@
 const {Router} = require('express')
 const request = require("request");
+const API = require("../configs/axios");
 const router = Router()
 
 
-router.get('/colobarators', (req, res) => {
-    res.render('pages/colobarators/index.ejs'); // Переход на страницу "Контакты"
+router.get('/colobarators', async (req, res) => {
+    let response = null;
+    let lng = req.getLocale();
+    try {
+        await API.get(`/collaborators-page?locale=${lng}&populate=deep`).then(
+            ({ data }) => (response = data)
+        );
+    } catch (err) {
+        console.log(err);
+    }
+
+    res.render('pages/colobarators/index.ejs' , {
+        data: response,
+    }); // Переход на страницу "Контакты"
 });
 
 
--- routes/projects.js
+++ routes/projects.js
@@ -1,12 +1,24 @@
 const {Router} = require('express')
 const request = require("request");
+const API = require("../configs/axios");
 const router = Router()
 
 
 
 
-router.get('/projects', (req, res) => {
-    res.render('pages/projects/index.ejs'); // Переход на страницу "Контакты"
+router.get('/projects', async (req, res) => {
+    let response = null;
+    let lng = req.getLocale();
+    try {
+        await API.get(`/projects-page?locale=${lng}&populate=deep`).then(
+            ({ data }) => (response = data)
+        );
+    } catch (err) {
+        console.log(err);
+    }
+    res.render('pages/projects/index.ejs', {
+        data: response,
+    });
 });
 
 
